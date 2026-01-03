ebfc75fcb4206b532d6bc505ebd8b9943fbe5dc9
wikigough <39881845+wikigough@users.noreply.github.com>
2026-01-03 16:08:47 +0000

    Photodiode experiment complete
    Added moving average for photodiode read. Made scaled brightness function actually scalable between brightness 255 and 66.
    Changed comparator input to match P2.5 rather than P2.4. Still needs a tidy in terms of documentation and potentially brightness function location.

--- space_earrings/earrings.c
+++ space_earrings/earrings.c
@@ -20,6 +20,7 @@ uint8_t battery_good_flag = 1;
 //private functions
 uint8_t get_scaled_brightness(uint8_t brightness);
 uint8_t brightness_check(void);
+uint8_t update_ma_size_8(uint8_t new_item, uint8_t* ring_buff, uint8_t* ring_buff_iter);
 
 void init_earrings(void)
 {
@@ -52,7 +53,9 @@ void init_earrings(void)
 void run_earrings(void)
 {
     uint16_t battery_voltage;
-    static uint8_t brightness = 160; // initial brightness setting, variable changed by photodiode measurement
+    static uint8_t brightness = 255; // initial brightness setting, variable changed by photodiode measurement
+    static uint8_t brightness_ring_buff[8] = {0};
+    static uint8_t brightness_ring_buff_iter = 0;
 
     while(1)
     {
@@ -82,16 +85,17 @@ void run_earrings(void)
                 timer_1s_flag_reset();
             }
             
-            // if photodiode connected, execute brightness check to adjust global brightness - comment out if photodiode not connected!
-            //uint8_t temp_brightness = brightness_check();
-            //brightness = get_scaled_brightness(temp_brightness);
+            // execute brightness check to adjust global brightness - comment out if photodiode not connected!
+            uint8_t temp_brightness = brightness_check();
+            uint8_t temp_brightness_ma = update_ma_size_8(temp_brightness, brightness_ring_buff, &brightness_ring_buff_iter);
+            brightness = get_scaled_brightness(temp_brightness_ma);
 
         }
         //for debug only
         if(get_switch_flag())
         {
             // do something here
-            
+            // DEBUG 
            
             // clear switch flag
             clear_switch_flag();
@@ -143,18 +147,25 @@ uint8_t brightness_check(void)
     reset_comp_low_to_high();
     enable_comp_interrupts();
 
-    uint8_t dac_settings[7] = {24, 20, 16, 12, 8, 4, 0};
+    // only going through some of the values to make it quicker. 
+    //uint8_t dac_settings[8] = {28,24, 20, 16, 12, 8, 4, 0};
+    //uint8_t dac_settings[8] = {63,55, 47, 39, 31, 23, 15, 7};
+    uint8_t size = 17;
+    uint8_t dac_settings[17] = {63, 59, 55, 51, 47, 43, 39, 35, 31, 27, 23, 19, 15, 11, 7, 3, 0};
     uint8_t i = 0;
 
     // we want to go from bright to dim - so decrease the DAC setting and look for a high to low transition.
     do{
         set_dac_multiplier(dac_settings[i]);
         i++;
-    } while (!get_comp_high_to_low() & (i<6));
+    } while (!get_comp_high_to_low() & (i<size));
 
     // set dac back to something really high so it is not constantly triggering. 
     set_dac_multiplier(63);
     disable_comp_interrupts();
+    
+    if (i==size) return dac_settings[0];
+    
     return dac_settings[i];
 
 }
@@ -169,17 +180,59 @@ uint8_t brightness_check(void)
  */
 uint8_t get_scaled_brightness(uint8_t brightness)
 {
-    uint8_t scaled_brightness;
-    //brightness = 30 is factor 1
-    //brightness = 0 is factor 0.8
+    
+    //brightness max = 255 as scaled to 63
+    //brightness lowest = 255-(63*3) = 66 as scaled to 0
     // this is only done once a second so can be relatively inefficient.
-    if (brightness >= 10)
+    /*
+    uint8_t scaled_brightness; 
+    if (brightness >= 32)
     {
         scaled_brightness = 255;
     }
     else
     {
-        scaled_brightness = 80;
+        scaled_brightness = 66;
     }
     return scaled_brightness;
+    */
+    int scaled_brightness = 66 + brightness * 3;
+    if (scaled_brightness > 255) scaled_brightness = 255;
+    return (uint8_t) scaled_brightness;
+
 }
+
+/**
+ * @brief Maintain a rolling average of the incoming brightness values.
+ * @ingroup EARRINGS_APP
+ * @param brightness Curre
+ * @return Return value as described in the detailed design.
+ * @note This is an internal helper; it is not exposed in the public header. Currently very simple.
+ */
+
+ uint8_t update_ma_size_8(uint8_t new_item, uint8_t* ring_buff, uint8_t* ring_buff_iter)
+ {
+    uint8_t ring_buff_size = 8;
+    // place new item into the ring_buff with the correct location. 
+    ring_buff[*ring_buff_iter] = new_item;
+
+    // update iterator for circular buffer
+    *ring_buff_iter += 1;
+    if (*ring_buff_iter >= ring_buff_size)
+    {
+        *ring_buff_iter = 0;
+    }
+
+    // get ma of the new buffer.
+    int sum = 0;
+    int j = 0;
+    for (j = 0; j<ring_buff_size; j++)
+    {
+        sum += ring_buff[j];
+    }
+
+    return (uint8_t)(sum >> 3);
+
+ }
+
+
